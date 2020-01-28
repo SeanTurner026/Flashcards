7e09991968afa82bb7f1cf5cb77e0bcae3631d9d
SeanTurner026 <sean.turner026@gmail.com>
2020-01-29 10:23:41 +1300

    Rewrote program using classes and click library

--- flashcards.py
+++ flashcards.py
@@ -1,122 +1,118 @@
 #!/usr/bin/env python
 """
-flashcard.py reads a yaml file containing flashcards, and presents the user with either the English
-or Spanish side. The user then presses [SPACEBAR] to reveal the translation, clear the screen, and
+flashcard.py reads a yaml file containing flashcards, and presents the user with either keys or
+values. The user then presses [SPACEBAR] to reveal the opposite, clear the screen, and
 present the next card. Exit the program with [CTRL] + [C].
 """
-import argparse
 import os
 import random
 import sys
 
+import click
 import keyboard
+import ruamel.yaml
 
 
-def load_deck(deck_file):
-    """Load a yaml file containing the flashcards
-
-    Returns a list containing lists in the following format:
-    [['word', 'translation'],
-     ['word', 'translation']]
+class Flashcards:
     """
-    with open(deck_file) as file:
-        # exclude yaml document header
-        if file.readline() is '---':
-            return [[line.split(':')[0], line.split(':')[-1][1:].split('\n')[0]]
-                    for line in file if line[0] != '#']
+    pass
+    """
 
-        return [[line.split(':')[0], line.split(':')[-1][1:].split('\n')[0]]
-                for line in file if line[0] != '#']
+    def __init__(self):
+        self.deck = ruamel.yaml.YAML()
+        self.card = ['key', 'value']
+
+    def load_deck(self, file):
+        """
+        load_deck() takes and loads yaml file a yaml file into memory, and also prints instructions
+        and how many cards are in the deck.
+
+        params
+        :file - local yaml file specified via cli
+        """
+        with open(file, mode='r') as deck:
+            self.deck = self.deck.load(deck)
+
+        print('Press [SPACEBAR] to advance. Exit at anytime with [CTRL] + [C]\n'
+              'There are {} cards in your deck.\n'.format(len(self.deck)))
+
+    def read_card(self):
+        """
+        read_card() selects a random card from the deck residing in memory, and prints the key in
+        bold. the function then waits for the user to hit [SPACEBAR] to proceed.
+        """
+        self.card = random.choice(
+            list(self.deck.items())
+        )
+        # bold terminal output
+        print('\033[1m' + self.card[0] + '\033[0m')
+        keyboard.wait('space')
+
+    def flip_card(self):
+        """
+        flip_card() checks to see if the value of the card is a yaml list or a string, and prints
+        the formatted output accordingly to the terminal. the function then waits for the user to
+        hit [SPACEBAR] to proceed.
+        """
+        if isinstance(self.card[1], ruamel.yaml.comments.CommentedSeq):
+            for item in self.card[1]:
+                print('» {}'.format(item))
+        else:
+            print('» {}'.format(
+                str(self.card[1].strip('\n'))
+            ))
+        keyboard.wait('space')
+        os.system('clear')
+
+    # def check_reverse(self, reverse):
+    #     """
+    #     check_reverse() looks to see if the reverse flag was passed as an argument, and reverses
+    #     the keys and values in the deck
+
+    #     params
+    #     :reverse - boolean cli flag
+    #     """
+    #     if reverse is True:
+    #         self.deck = [
+    #             [card[1], card[0]
+    #              ] for card in self.deck]
 
 
-def read_card(card_deck):
-    """Print one side of a card from the deck"""
-    card = random.choice(card_deck)
-    print(card[0].title())
-    keyboard.wait('space')
-    return card
+@click.command()
+@click.option('-f', '--file', help='specifies yaml file to use')
+# @click.option('-r', '--reverse',
+#               default=False, help='displays values prompting user to guess keys'
+#               )
+def main(file):
+    """
+    main() checks that the necessary flags were passed, that the program was run as sudo, and
+    executes the workflow.
 
-
-def flip_card(card):
-    """Print the translation of the card from read_card()"""
-    print('» {}'.format(card[1].capitalize()))
-    keyboard.wait('space')
+    params
+    :file - local yaml file specified via cli
+    :reverse - boolean cli flag
+    """
+    if file is None:
+        sys.exit('Please pass a yaml file to the application with the usage -f file.yaml')
+    if os.getuid() != 0:
+        sys.exit(
+            'The keyboard module requires super user privileges. Please run the program as sudo'
+            )
+    flashcards = Flashcards()
+    flashcards.load_deck(file)
+    # flashcards.check_reverse(reverse)
     os.system('clear')
-    return None
 
-
-def run_deck(args):
-    """Select language and call read_card() and flip_card() alternatively"""
-    deck = load_deck(args['deck'])
-    if args['english'] is True:
-        # reverse the yaml keys with values so that the user guesses the Spanish word
-        deck = [[card[1], card[0]] for card in deck]
-
-    os.system('clear')
-    print('Press [SPACEBAR] to advance. Exit at anytime with [CTRL] + [C]\n'
-          'There are {} cards in your deck.\n'.format(len(deck)))
     try:
         while True:
-            card = read_card(deck)
-            flip_card(card)
+            flashcards.read_card()
+            flashcards.flip_card()
 
     except KeyboardInterrupt:
         # removes '^C' from terminal output
         os.system('clear')
         sys.exit(0)
 
-    return None
-
-
-def parse_command_line():
-    """Return the command line arguments as a dictionary"""
-    parser = argparse.ArgumentParser(description='Flashcard program')
-
-    parser.add_argument('-d', '--deck', nargs='?', default='cards.yaml',
-                        help='specifies deck to use')
-
-    parser.add_argument('-e', '--english', action='store_true', default=False,
-                        help='specifies english to spanish mode')
-
-    parser.add_argument('-s', '--spanish', action='store_true', default=False,
-                        help='specifies spanish to english mode')
-
-    return vars(parser.parse_args())
-
-
-def check_parsed_arguments(args):
-    """Executes flow control to check cli flags and ensure that the program runs properly"""
-    if os.getuid() != 0:
-        print('The keyboard module requires super user privileges. Please run the program as sudo')
-        sys.exit(0)
-
-    if args.get('deck') is None:
-        print('Please pass in a yaml flashcard deck after the \'-d\' flag')
-        sys.exit(0)
-
-    if 'deck' not in args:
-        print('Please pass in a yaml flashcard deck after the \'-d\' flag')
-        sys.exit(0)
-
-    if '.yaml' not in args.get('deck'):
-        print('Please pass in a yaml flashcard deck after the \'-d\' flag')
-        sys.exit(0)
-
-    elif args.get('spanish') is False:
-        if args.get('english') is False:
-            print('Please indicate language by passing in one of the \'-s\' or \'-e\' flags.')
-            sys.exit(0)
-
-    return None
-
-
-def main():
-    """Only run when the program is run directly"""
-    args = parse_command_line()
-    check_parsed_arguments(args)
-    run_deck(args)
-
 
 if __name__ == "__main__":
-
     main()
